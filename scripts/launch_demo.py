56a5d4a82916ffe67a67ee2d75e9cc81d55af179
NOIRUED <ueda@komachi.live>
2022-08-10 14:38:41 +0900

    Fixed Bugs

--- scripts/launch_demo.py
+++ scripts/launch_demo.py
@@ -10,15 +10,11 @@ t5_tokenizer = AutoTokenizer.from_pretrained(T5_PATH, use_fast=False)
 t5_config = AutoConfig.from_pretrained(T5_PATH)
 t5_mlm = T5ForConditionalGeneration.from_pretrained(T5_PATH, config=t5_config).to(DEVICE)
 
-def _filter(output):
-    _txt = t5_tokenizer.decode(output, skip_special_tokens=True)
-    return _txt
-
 text = st.text_area('Text to Analyze', '''Input a text''')
 start_analyse = st.button("Analyse")
 
 if start_analyse:
     input_ids = t5_tokenizer(text, add_special_tokens=True, return_tensors="pt").input_ids.to(DEVICE)
     outputs = t5_mlm.generate(input_ids=input_ids)
-    output = list(map(_filter, outputs))
-    st.text_area("", output[0])
+    _txt = t5_tokenizer.decode(outputs[0], skip_special_tokens=True)
+    st.text_area("", _txt)
